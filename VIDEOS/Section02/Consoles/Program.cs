faa237d16e411467e4ff894da8c25f47cacbece2
Muhammad Faza Akbar <akbarfaza222@gmail.com>
2023-02-18 09:13:36 +0800

    moving to VIDEOS

---
+++ VIDEOS/Section02/Consoles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Consoles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Welcome");
+            Console.Write("Hello There");           // print without a new line
+            Console.WriteLine("s");
+
+            // using the ReadLine()
+            Console.Write("Enter a string and press Enter: ");
+            string readInput = Console.ReadLine();
+            Console.WriteLine("You have entered {0}", readInput);
+
+            // using Read()
+            Console.Write("Enter a string and press Enter: ");
+            int asciiValue = Console.Read();
+            Console.WriteLine("ASCII value is {0}", asciiValue);
+            Console.ReadKey();
+        }
+    }
+}
